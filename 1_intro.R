16dd8fdb596ff39c8093a8dabc89c8a3a150159e
anabella005 <43819061+anabella005@users.noreply.github.com>
2019-10-22 09:52:03 -0300

    update

--- 1_intro.R
+++ 1_intro.R
@@ -1,20 +1,18 @@
 str(EBV)
-colnames(EBV)[15]<- "PP"
-colnames(EBV)[3]<-"ISOM"
-colnames(EBV)[4]<-"FNACNUMERO"
-colnames(EBV)[5]<-"SEXOLETRA"
-colnames(EBV)[6]<-"ORI"
-colnames(EBV)[7]<-"TIENELAC"
-colnames(EBV)[8]<-"HIJASLECDIR"
-colnames(EBV)[9]<-"LECHE"
-colnames(EBV)[10]<-"GRASA"
-colnames(EBV)[11]<-"PGRASA"
-colnames(EBV)[12]<-"PROT"
-colnames(EBV)[13]<-"PPROT"
-colnames(EBV)[14]<-"CONFP"
+colnames(EBV)[1]<-"ISO" # nombre del registro en Argentina
+colnames(EBV)[2]<-"ISOP" #nombre del registro del padre
+colnames(EBV)[3]<-"ISOM" #nombre del registro de la madre
+colnames(EBV)[4]<-"FNACNUMERO" #fecha de nacimiento
+colnames(EBV)[5]<-"SEXOLETRA" #sexo
+colnames(EBV)[6]<-"ORI" #origen del registro internacional del animal
+colnames(EBV)[7]<-"TIENELAC" #1 si la vaca tuvo lactancia para evaluacion genetica
+colnames(EBV)[8]<-"HIJASLECDIR" #hijas del  macho validas para evaluacion genetica
+colnames(EBV)[9]<-"LECHE" #valor genetico leche
+colnames(EBV)[10]<-"GRASA" #idem grasa
+colnames(EBV)[11]<-"PGRASA" #porcentaje grasa
+colnames(EBV)[12]<-"PROT" #proteina
+colnames(EBV)[13]<-"PPROT" #porcentaje proteina
+colnames(EBV)[14]<-"CONFP"#confiabilidad del valor genético
 str(EBV)
 
-EBV<- within(EBV, {
-  PP <- NULL
-})
 

4181b715f08a0d64212024eb637060b772ac4ea4
anabella005 <43819061+anabella005@users.noreply.github.com>
2019-10-29 15:28:37 -0300

    update

--- 3_estructura de datos.R
+++ 3_estructura de datos.R
@@ -35,13 +35,51 @@ fct_explicit_na(EBV$ISOM)
 
 library(forcats)
 
-int_gen1930<-decada1930 %>% 
+padres<-EBV %>% 
   group_by(fct_explicit_na(ISOP)) %>% 
-  summarise(FNAC_min = min (FNACNUMERO),
-            FNAC_mean = mean (FNACNUMERO),
-            FNAC_max = max (FNACNUMERO),
-            hijos = length(FNACNUMERO))
+  summarise(hijos = length(FNACNUMERO))
+
+madres<-EBV %>% 
+  group_by(fct_explicit_na(ISOM)) %>% 
+  summarise(hijos = length(FNACNUMERO))
 
 #Para pasar el conjunto de datos de R a txt:
 write.table(EBV, "C:/Users/Ana/Desktop/tesis FG/R/EBV.txt", sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE, 
             na = "NA")
+
+#padres mas influyentes: 
+#mayor numero de hijos?
+padres<-EBV %>% 
+  group_by(fct_explicit_na(ISOP)) %>% 
+  summarise(hijos = length(FNACNUMERO))
+
+madres<-EBV %>% 
+  group_by(fct_explicit_na(ISOM)) %>% 
+  summarise(hijos = length(FNACNUMERO))
+#mayor valor de EBV?
+EBV_padres<-EBV %>% 
+  group_by(fct_explicit_na(ISOP)) %>% 
+  summarise(hijos = length(FNACNUMERO),
+            ORI = mean(ORI),
+            min.leche = min(LECHE),
+            mean.leche = mean(LECHE),
+            max.leche = max(LECHE),
+            min.prot = min(PROT),
+            mean.prot = mean(PROT),
+            max.prot = max(PROT),
+            min.grasa = min(GRASA),
+            mean.grasa = mean(GRASA),
+            max.grasa = max(GRASA))
+EBV_madres<-EBV %>% 
+  group_by(fct_explicit_na(ISOM)) %>% 
+  summarise(hijos = length(FNACNUMERO),
+            min.leche = min(LECHE),
+            mean.leche = mean(LECHE),
+            max.leche = max(LECHE),
+            min.prot = min(PROT),
+            mean.prot = mean(PROT),
+            max.prot = max(PROT),
+            min.grasa = min(GRASA),
+            mean.grasa = mean(GRASA),
+            max.grasa = max(GRASA))
+
